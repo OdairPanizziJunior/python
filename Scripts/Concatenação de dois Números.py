951511a39a29d9fa9b4032f5973091a67d8a92a7
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2022-04-18 16:15:02 -0300

    Delete Scripts directory

--- Scripts/Concatenação de dois Números.py
+++
@@ -1,9 +0,0 @@
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite outro número: "))
-s = n1+n2
-
-print("A soma de", n1, "+", n2, "é", s)
-
-print("A soma de {} + {} é {}".format(n1,n2,s))
-
-print(type(n1))
