0f2d046e85759c587423a558e993b9154d8f2207
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2022-04-18 17:04:12 -0300

    Update Soma de dois Números.py

--- Scripts/Soma de dois Números.py
+++ Scripts/Soma de dois Números.py
@@ -1,9 +1,7 @@
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite outro número: "))
-s = n1+n2
+n1 = int(input("Digite um número: ")) #chama a função de input e guarda na variável n1
+n2 = int(input("Digite outro número: ")) #chama a função de input e guarda na variável n2
+s = n1+n2 #Variável "s" recebe a coma de "n1" + "n2"
 
-print("A soma de", n1, "+", n2, "é", s)
+print("A soma de", n1, "+", n2, "é", s) #exemplo básico de concatenação com PY
 
-print("A soma de {} + {} é {}".format(n1,n2,s))
-
-print(type(n1))
+print("A soma de {} + {} é {}".format(n1,n2,s)) #exemplo mais avançado de concatenação com PY
