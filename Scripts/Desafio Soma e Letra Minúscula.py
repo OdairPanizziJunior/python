113863d7b5f8ae9edf9f232663e8e2f9c4dc819a
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2022-04-18 16:24:48 -0300

    Add files via upload

---
+++ Scripts/Desafio Soma e Letra Minúscula.py
@@ -0,0 +1,13 @@
+print("DESAFIO 1 AULA 06")
+print("")
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+s = n1+n2
+print("")
+print("A soma de", n1, "+", n2, "é", s)
+print("A soma de {} + {} é {}".format(n1,n2,s))
+print("")
+print("DESAFIO 2 AULA 06")
+n = input("digite algo: ")
+print(n.islower(), ", pois está em letra minúscula")
+print(n.isnumeric(), ", pois não é um número" )
