679b56bcf1fe628d7535585ed219e15546334080
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2022-04-18 17:01:11 -0300

    Update Desafio Soma e Letra Minúscula.py

--- Scripts/Desafio Soma e Letra Minúscula.py
+++ Scripts/Desafio Soma e Letra Minúscula.py
@@ -1,4 +1,5 @@
-print("DESAFIO 1 AULA 06")
+#DESAFIO UM
+print("DESAFIO 1")
 print("")
 n1 = int(input("Digite um número: "))
 n2 = int(input("Digite outro número: "))
@@ -7,7 +8,9 @@ print("")
 print("A soma de", n1, "+", n2, "é", s)
 print("A soma de {} + {} é {}".format(n1,n2,s))
 print("")
-print("DESAFIO 2 AULA 06")
+
+#DESAFIO DOIS
+print("DESAFIO 2")
 n = input("digite algo: ") 
 print(n.islower(), ", pois está em letra minúscula") #Se letras minúsculas, retorna TRUE
-print(n.isnumeric(), ", pois não é um número" ) #Se soma inicial conter dois números, retorna TRUE se não FALSE e exibe mensagem.
+print(n.isnumeric(), ", pois NÃO é um número" ) #retorna FALSE e exibe a mensagem se "n" receber Letras.
